ff1bccf7dd8a5a1dc1ae76add1f429a576ca2c5b
André <andrerrr@outlook.de>
2019-02-13 11:04:59 +0100

    Entity Klasse erstellt

---
+++ FRANET.PmS.PDA/FRANET.PmS.PDA.Model/Entity.vb
@@ -0,0 +1,8 @@
+Public MustInherit Class Entity
+    Public Property Id As Integer
+    Public Property Created As Date
+    Public Property CreatedBy As String
+    Public Property Modified As Date
+    Public Property ModifiedBy As String
+    Public Property IsDeleted As Boolean
+End Class
